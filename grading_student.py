7e5109c2c4521568b4567d34a314b6a9e7aab200
anonymousdomain <dawityitagesu@gmail.com>
2022-12-24 05:19:52 +0300

    the code submitted to hackerrank

--- grading_student.py
+++ grading_student.py
@@ -1,26 +1,53 @@
-class Grade:
-    def round_grade(self, grades):
-        for i in range(0, len(grades)):
-            if grades[i] < 38:
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'gradingStudents' function below.
+#
+# The function is expected to return an INTEGER_ARRAY.
+# The function accepts INTEGER_ARRAY grades as parameter.
+#
+
+def gradingStudents(grades):
+    # Write your code here
+    for i in range(0, len(grades)):
+        if grades[i] < 38:
+            continue
+
+        else:
+            temp = grades[i]
+            te = grades[i] % 5
+            if te == 3:
+                temp = temp+2
+                grades[i] = temp
+            elif te == 4:
+                temp = temp+1
+                grades[i] = temp
+            else:
                 continue
 
-            else:
-                temp = grades[i]
-                te = grades[i] % 5
-                if te == 3:
-                    temp = temp+2
-                    grades[i] = temp
-                elif te == 4:
-                    temp = temp+1
-                    grades[i] = temp
-                else:
-                    continue
-
-        return grades
+    return grades
 
 
-g = Grade()
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
 
-s = g.round_grade([34, 54, 67])
+    grades_count = int(input().strip())
 
-print(s)
+    grades = []
+
+    for _ in range(grades_count):
+        grades_item = int(input().strip())
+        grades.append(grades_item)
+
+    result = gradingStudents(grades)
+
+    fptr.write('\n'.join(map(str, result)))
+    fptr.write('\n')
+
+    fptr.close()
