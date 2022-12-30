0c348acc2e32382d898298b6c6f5ba6f1b7cbe18
anonymousdomain <dawityitagesu@gmail.com>
2022-12-30 09:02:36 +0300

    selection sort challange from geeks for geeks

---
+++ SelectionSort.java
@@ -0,0 +1,61 @@
+//{ Driver Code Starts
+    import java.util.*;
+
+    class GFG
+    {
+        public static void main(String args[])
+        {
+            Scanner sc = new Scanner(System.in);
+            int t = sc.nextInt();
+            while(t>0)
+            {
+                int n = sc.nextInt();
+                int arr[] = new int[n]; 
+                for(int i=0;i<n;i++)
+                {
+                    arr[i] = sc.nextInt();
+                }
+            
+                Solution obj = new Solution();
+                obj.selectionSort(arr, n);
+                
+                for(int i=0;i<n;i++)
+                    System.out.print(arr[i]+" ");
+                System.out.println();
+                t--;
+            }
+            
+        }
+    }
+    
+    // } Driver Code Ends
+    
+    
+    
+    class Solution {
+        
+        int select(int arr[], int n) {
+            // code here such that selectionSort() sorts arr[]
+         
+    return arr[n];
+        }
+    
+        void selectionSort(int arr[], int n) {
+        
+           for(int i=0;i<n-1;i++){
+               
+                int  index=i;
+                for(int j=i+1;j<n;j++){
+                    if(arr[j]<select(arr,i)){
+                        index=j;
+                    }
+                }
+                
+              if(index!=i){
+                int temp=arr[index];
+                arr[index]=arr[i];
+                arr[i]=temp;
+              }
+            }
+        }
+    }
