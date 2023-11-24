f78125ba38c2cbe69faa88494fcbe196e7e07aaf
Dawit Mekonnen <dawityitagesu@gmail.com>
2023-11-24 03:44:39 +0300

    prime numbers

---
+++ leetcode/204.count-primes.py
@@ -0,0 +1,28 @@
+#
+# @lc app=leetcode id=204 lang=python3
+#
+# [204] Count Primes
+#
+
+# @lc code=start
+class Solution:
+    def countPrimes(self, n: int) -> int:
+        if n <= 2:
+            return 0
+
+        is_prime = [True] * n
+        is_prime[0] = is_prime[1] = False  # 0 and 1 are not primes
+        
+       
+        for i in range(2, int(n**0.5) + 1):
+            if is_prime[i]:
+                for j in range(i*i, n, i):
+                    is_prime[j] = False
+        
+        # Count the number of primes
+        count = sum(is_prime)
+        return count
+
+        
+# @lc code=end
+
